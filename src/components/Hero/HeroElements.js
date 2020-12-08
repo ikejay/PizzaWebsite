4d8963d7d152fc886d15e17b3f7242e7e22e0c27
ikejay <Succianan195@gmail.com>
2020-12-08 11:48:37 +0000

    small changes

---
+++ src/components/Hero/HeroElements.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import imgBg from '../../images/pizza-3.jpg';
+
+
+export const HeroContainer = styled.div`
+   background: linear-gradient(to right, rgba(0,0,0,0.7), rgba(0,0,0,0.1), 
+   url (${ImgBg}));
+  `;
